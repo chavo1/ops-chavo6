e955467d56356b672d1d4f8107276e31a6db3993
chavo1 <42143239+chavo1@users.noreply.github.com>
2018-08-18 16:42:29 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,39 @@
-resource "null_resource" "helloWorld1" {
-  provisioner "local-exec" {
-    command = "echo hello world1"
-  }
+# 2 repos with hardcoded names
+resource "github_repository" "example11" {
+  name        = "example11"
+  description = "My awesome codebase"
 }
 
-resource "null_resource" "helloWorld2" {
-  provisioner "local-exec" {
-    command = "echo hello world2"
-  }
+resource "github_repository" "example22" {
+  name        = "example22"
+  description = "My awesome codebase"
 }
 
-resource "null_resource" "helloWorld3" {
-  provisioner "local-exec" {
-    command = "echo hello world3"
-  }
+# We use random_pet to generate a random name
+resource "random_pet" "random_repo11" {
+  length    = "4"
+  separator = "-"
+}
+
+resource "random_pet" "random_repo22" {
+  length    = "4"
+  separator = "-"
+}
+
+# We generate 2 dynamic repos with the generated random names
+resource "github_repository" "random11" {
+  name        = "${random_pet.random_repo11.id}"
+  description = "My awesome pet project"
+}
+
+resource "github_repository" "random22" {
+  name        = "${random_pet.random_repo22.id}"
+  description = "My awesome pet project"
+}
+
+# We use count to generate multiple repos
+resource "github_repository" "multiples" {
+  count       = "${var.repo_count}"
+  name        = "multiple${count.index}"
+  description = "My awesome codebase"
 }
